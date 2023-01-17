83f38f1a0c984870da2e99c08ef901cc6b68a9a2
Jevin Sweval <jevinsweval@gmail.com>
2023-01-17 13:28:16 -0500

    initial swift template

---
+++ Surf/Surf/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Surf
+//
+//  Created by Jevin Sweval on 1/17/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
