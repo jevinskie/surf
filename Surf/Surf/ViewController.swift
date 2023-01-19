898527489d890b78200e99aa1d15a0ab94600bd9
Jevin Sweval <jevinsweval@gmail.com>
2023-01-18 22:04:49 -0500

    broken rotator wip. added reactiveswift/cocoa

--- Surf/Surf/ViewController.swift
+++ Surf/Surf/ViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import Cocoa
+import ReactiveCocoa
+import ReactiveSwift
+
+extension Reactive where Base: CALayer {
+
+    // Provided for cross-platform compatibility
+
+    // public var transformValue: BindingTarget<CATransform3D> { return transform }
+    // public var transformValues: Signal<CATransform3D, Never> { return floatValues }
+}
 
 class ViewController: NSViewController {
+    @IBOutlet weak var label: NSTextField!
+    @IBOutlet weak var slider: NSSlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let (signal, observer) = Signal<String, Never>.pipe()
+        // signal
+        slider.reactive.values
+            .observeValues { value in print(value) }
+        label.layer?.transform = CATransform3DMakeAffineTransform(CGAffineTransform(rotationAngle: 0.5 * 2 * 3.14))
+        // label.layer?.reactive.transform <~ slider.reactive.values.map { value in CGAffineTransform(rotationAngle: value * 2 * 3.14) }
     }
 
     override var representedObject: Any? {
