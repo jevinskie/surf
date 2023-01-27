96036ec782e9640d24492c42c794e5da80ae2fc6
Jevin Sweval <jevinsweval@gmail.com>
2023-01-26 21:09:58 -0500

    lol

--- libsurf/lib/vcd-parser.cpp
+++ libsurf/lib/vcd-parser.cpp
@@ -14,6 +14,8 @@
 
 #include <concurrencpp/concurrencpp.h>
 
+// need combination_duplicate for decl parsing
+
 using namespace VCDTypes;
 
 namespace surf {
@@ -168,9 +170,8 @@ struct document {
     static constexpr auto c = b | dsl::else_ >> dsl::try_(dsl::else_ >> cs);
     static constexpr auto d = c | dsl::else_ >> ef;
     static constexpr auto rule =
-        dsl::p<opt_decl_list> +
-        dsl::if_(dsl::peek(dsl::p<opt_sim_cmd_list>) >> dsl::try_(dsl::p<opt_sim_cmd_list>)) +
-        dsl::eof;
+        dsl::if_(dsl::peek(end_defs_decl_pair) >> dsl::try_(dsl::p<opt_decl_list>)) +
+        dsl::if_(dsl::peek_not(dsl::eof) >> dsl::try_(dsl::p<opt_sim_cmd_list>)) + dsl::eof;
 #endif
 
     // static constexpr auto rule = dsl::try_(decls) + dsl::try_(cmds) + dsl::eof;
@@ -183,11 +184,11 @@ struct document {
         [](std::vector<int> &&decls) {
             // DUMP_STACK("single decls arg");
             fmt::print("single decls arg\n");
-            return Document{};
+            return Document{.nums = std::move(decls)};
         },
         [](std::vector<std::string> &&sim_cmds) {
             fmt::print("single sim_cmds arg\n");
-            return Document{};
+            return Document{.words = std::move(sim_cmds)};
         },
         [](std::vector<int> &&decls, std::vector<std::string> &&sim_cmds) {
             fmt::print("decls and sim_cmds\n");
